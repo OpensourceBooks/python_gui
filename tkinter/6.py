63acc2ee1c7503ead3fc37d20550df295a8eee7f
cr4fun <cr4fun@outlook.com>
2018-09-05 17:43:50 +0800

    up

---
+++ tkinter/6.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+class App():
+    def __init__(self):
+
+        self.number = 0
+
+    def widget(self):
+        self.root = Tk()
+        self.root.title("app")
+        img = Image.open('ques.png')
+        photo = ImageTk.PhotoImage(img)
+        self.img_label = Label(self.root, image=photo)
+        self.button = Button(self.root, text="Hello, world!",command = self.click,width=50,height=5)
+
+    def pack(self):
+        self.button.pack()
+        self.img_label.pack()
+
+    #events
+    def click(self):
+        self.number += 1
+        self.button["text"] = self.number
+
+    #run
+    def run(self):
+        self.widget()
+        self.pack()
+        self.root.mainloop()
+
+App().run()
